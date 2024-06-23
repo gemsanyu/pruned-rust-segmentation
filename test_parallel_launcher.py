f73e7df6d2932fb13b27aab0007f11d805553215
gemilang <git.gemilang.santiyuda@gmail.com>
2024-06-23 22:17:14 +0800

    test in parallel

--- test_parallel_launcher.py
+++ test_parallel_launcher.py
@@ -1,7 +1,7 @@
 import multiprocessing as mp
 import subprocess
 
-def test_proc(arch, encoder, title):
+def test_proc(arch, encoder, title, dataset):
     process_args = ["python",
                     "test.py",
                     "--arch",
@@ -11,7 +11,7 @@ def test_proc(arch, encoder, title):
                     "--title",
                     title,
                     "--dataset",
-                    "NEA",
+                    dataset,
                     "--device",
                     "cpu",]
     subprocess.run(process_args)
@@ -20,7 +20,8 @@ def test_proc(arch, encoder, title):
 if __name__ == "__main__":
     arch_list = ["fpn", "manet", "deeplabv3", "unet", "linknet", "unet++"]
     encoder = "mobilenet_v2"
-    title = "exp_1"
-    args_list = [(arch, encoder, title) for arch in arch_list]
+    title = ""
+    datasets = ["NEA", "CCSC"]
+    args_list = [(arch, encoder, title, dataset) for dataset in datasets for arch in arch_list]
     with mp.Pool(6) as pool:
        pool.starmap(test_proc, args_list)
